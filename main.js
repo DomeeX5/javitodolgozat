11dfad6be9e3c53a474626ea5c8163287d8f1e13
Petrik Diak <diak@petrik.hu>
2023-10-16 15:59:34 +0200

    javascript megírása, kész feladat

--- main.js
+++ main.js
@@ -2,4 +2,20 @@ import './style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 
-
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('kepurl').addEventListener('change', () => {
+    document.getElementById('kep').src = document.getElementById('kepurl').value;
+  })
+  document.getElementById('betumeret').addEventListener('input', () => {
+    document.getElementById('szoveg').style.fontSize = document.getElementById('betumeret').value + 'pt';
+  })
+  document.getElementById('sorkoz').addEventListener('input', () => {
+    document.getElementById('szoveg').style.lineHeight = document.getElementById('sorkoz').value + 'em';
+  })
+  document.getElementById('betuszin').addEventListener('input', () => {
+    document.getElementById('szoveg').style.color = document.getElementById('betuszin').value;
+  })
+  document.getElementById('gomb').addEventListener('click', () => {
+    document.getElementById('kep').classList.toggle('jobbrausztatas');
+  })
+});
